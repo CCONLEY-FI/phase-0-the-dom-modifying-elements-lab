5edafaf9df1d30f2600c1b53d68f36c339f4ff0a
Chris <conley.christopher0@gmail.com>
2023-12-06 13:37:01 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Chris is the champion";
+
+const mainElement = document.querySelector('#main');
+mainElement.appendChild(newHeader);
+mainElement.remove('main');
